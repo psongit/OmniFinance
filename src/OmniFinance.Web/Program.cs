56047f5c5c551a7067bacdd82bbc6d2fc4ec69d4
Prakash Somasundaram <prakash.somasundaram@ascertra.com>
2024-03-11 10:08:21 +0530

    Enabled basic Azure AD Authentication

--- src/OmniFinance.Web/Program.cs
+++ src/OmniFinance.Web/Program.cs
@@ -1,44 +1,156 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using OmniFinance.Web.Client.Pages;
 using OmniFinance.Web.Components;
-using OmniFinance.Web.Components.Account;
-using OmniFinance.Web.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using OmniFinance.Web.Security;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("MicrosoftOidc")
+    .AddOpenIdConnect("MicrosoftOidc", oidcOptions =>
+    {
+        // For the following OIDC settings, any line that's commented out
+        // represents a DEFAULT setting. If you adopt the default, you can
+        // remove the line if you wish.
+
+        // ........................................................................
+        // The OIDC handler must use a sign-in scheme capable of persisting 
+        // user credentials across requests.
+
+        oidcOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        // ........................................................................
+
+        // ........................................................................
+        // The "openid" and "profile" scopes are required for the OIDC handler 
+        // and included by default. You should enable these scopes here if scopes 
+        // are provided by "Authentication:Schemes:MicrosoftOidc:Scope" 
+        // configuration because configuration may overwrite the scopes collection.
+
+        //oidcOptions.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+        // ........................................................................
+
+        // ........................................................................
+        // SaveTokens is set to false by default because tokens aren't required
+        // by the app to make additional external API requests.
+
+        //oidcOptions.SaveTokens = false;
+        // ........................................................................
+
+        // ........................................................................
+        // The following paths must match the redirect and post logout redirect 
+        // paths configured when registering the application with the OIDC provider. 
+        // For Microsoft Entra ID, this is accomplished through the "Authentication" 
+        // blade of the application's registration in the Azure portal. Both the
+        // signin and signout paths must be registered as Redirect URIs. The default 
+        // values are "/signin-oidc" and "/signout-callback-oidc".
+        // Microsoft Identity currently only redirects back to the 
+        // SignedOutCallbackPath if authority is 
+        // https://login.microsoftonline.com/{TENANT ID}/v2.0/ as it is above. 
+        // You can use the "common" authority instead, and logout redirects back to 
+        // the Blazor app. For more information, see 
+        // https://github.com/AzureAD/microsoft-authentication-library-for-js/issues/5783
+
+        //oidcOptions.CallbackPath = new PathString("/signin-oidc");
+        //oidcOptions.SignedOutCallbackPath = new PathString("/signout-callback-oidc");
+        // ........................................................................
+
+        // ........................................................................
+        // The RemoteSignOutPath is the "Front-channel logout URL" for remote single 
+        // sign-out. The default value is "/signout-oidc".
+
+        //oidcOptions.RemoteSignOutPath = new PathString("/signout-oidc");
+        // ........................................................................
+
+        // ........................................................................
+        // The "offline_access" scope is required for the refresh token.
+
+        oidcOptions.Scope.Add(OpenIdConnectScope.OfflineAccess);
+        // ........................................................................
+
+        // ........................................................................
+        // The following example Authority is configured for Microsoft Entra ID
+        // and a single-tenant application registration. Set the {TENANT ID} 
+        // placeholder to the Tenant ID. The "common" Authority 
+        // https://login.microsoftonline.com/common/v2.0/ should be used 
+        // for multi-tenant apps. You can also use the "common" Authority for 
+        // single-tenant apps, but it requires a custom IssuerValidator as shown 
+        // in the comments below. 
+
+        oidcOptions.Authority = "https://login.microsoftonline.com/95ac2b9b-4c55-4774-92c7-d916e3504027/v2.0/";
+        // ........................................................................
+
+        // ........................................................................
+        // Set the Client ID for the app. Set the {CLIENT ID} placeholder to
+        // the Client ID.
+
+        oidcOptions.ClientId = "425912e2-09be-45b8-bda8-8cb51dbbb8a2";
+        // ........................................................................
+
+        // ........................................................................
+        // ClientSecret shouldn't be compiled into the application assembly or 
+        // checked into source control. Instead adopt User Secrets, Azure KeyVault, 
+        // or an environment variable to supply the value. Authentication scheme 
+        // configuration is automatically read from 
+        // "Authentication:Schemes:{SchemeName}:{PropertyName}", so ClientSecret is 
+        // for OIDC configuration is automatically read from 
+        // "Authentication:Schemes:MicrosoftOidc:ClientSecret" configuration.
+
+        //oidcOptions.ClientSecret = "{PREFER NOT SETTING THIS HERE}";
+        // ........................................................................
+
+        // ........................................................................
+        // Setting ResponseType to "code" configures the OIDC handler to use 
+        // authorization code flow. Implicit grants and hybrid flows are unnecessary
+        // in this mode. In a Microsoft Entra ID app registration, you don't need to 
+        // select either box for the authorization endpoint to return access tokens 
+        // or ID tokens. The OIDC handler automatically requests the appropriate 
+        // tokens using the code returned from the authorization endpoint.
+
+        oidcOptions.ResponseType = OpenIdConnectResponseType.IdTokenToken;
+        // ........................................................................
+
+        // ........................................................................
+        // Many OIDC servers use "name" and "role" rather than the SOAP/WS-Fed 
+        // defaults in ClaimTypes. If you don't use ClaimTypes, mapping inbound 
+        // claims to ASP.NET Core's ClaimTypes isn't necessary.
+
+        oidcOptions.MapInboundClaims = false;
+        oidcOptions.TokenValidationParameters.NameClaimType = JwtRegisteredClaimNames.Name;
+        oidcOptions.TokenValidationParameters.RoleClaimType = "role";
+        // ........................................................................
+
+        // ........................................................................
+        // Many OIDC providers work with the default issuer validator, but the
+        // configuration must account for the issuer parameterized with "{TENANT ID}" 
+        // returned by the "common" endpoint's /.well-known/openid-configuration
+        // For more information, see
+        // https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/issues/1731
+
+        //var microsoftIssuerValidator = AadIssuerValidator.GetAadIssuerValidator(oidcOptions.Authority);
+        //oidcOptions.TokenValidationParameters.IssuerValidator = microsoftIssuerValidator.Validate;
+        // ........................................................................
+    })
+    .AddCookie("Cookies");
+
+// This attaches a cookie OnValidatePrincipal callback to get a new access token when the current one expires, and
+// reissue a cookie with the new access token saved inside. If the refresh fails, the user will be signed out.
+builder.Services.ConfigureCookieOidcRefresh("Cookies", "MicrosoftOidc");
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCascadingAuthenticationState();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
-
 builder.Services.AddFluentUIComponents();
 
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingAuthenticationStateProvider>();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -46,7 +158,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
-    app.UseMigrationsEndPoint();
 }
 else
 {
@@ -65,7 +176,6 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Counter).Assembly);
 
-// Add additional endpoints required by the Identity /Account Razor components.
-app.MapAdditionalIdentityEndpoints();
+app.MapGroup("/authentication").MapLoginAndLogout();
 
 app.Run();
